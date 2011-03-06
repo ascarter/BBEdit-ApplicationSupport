701c3cce82fcd71e0a0623248b19019567586f03
ascarter <ascarter@gmail.com>
2011-03-06 12:05:12 -0800

    Init script to add empty default BBEdit support directories

---
+++ README.markdown
@@ -0,0 +1,12 @@
+BBEdit Application Support
+==========================
+
+BBEdit extensions, tags, scripts, and other helpers.
+
+# Install
+
+	git clone git@github.com:ascarter/BBEdit-ApplicationSupport.git ~/Library/Application\ Support/BBEdit
+	cd ~/Library/Application\ Support/BBEdit
+	./init.sh
+
+The `init.sh` script will create all the empty directories for BBEdit's support directory if they don't already exist.
---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#
+# Create all BBEdit support directories if they don't already exist
+# Run this after cloning BBEdit-ApplicationSupport to
+# ~/Library/Application Support/BBEdit
+#
+
+mkdir -p Attachment Scripts
+mkdir -p Language Modules
+mkdir -p Scripts
+mkdir -p Unix Support
+mkdir -p Clippings
+mkdir -p Menu Scripts
+mkdir -p Startup Items
+mkdir -p Completion Data
+mkdir -p Plug-Ins
+mkdir -p Stationery
+mkdir -p HTML Templates
+mkdir -p Text Factories
+mkdir -p Clippings/Universal Items
