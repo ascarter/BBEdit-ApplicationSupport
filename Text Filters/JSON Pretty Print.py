9a4d88a9b01153e4a7cec5c0fffc7ed11e27a460
Andrew Carter <ascarter@gmail.com>
2011-12-08 07:09:24 -0800

    Text fitlers - JSON and JavaScript
    Beautify scripts for JSON and JavaScript

---
+++ Text Filters/JSON Pretty Print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import fileinput
+import json
+
+if __name__ == "__main__":
+  jsonStr = ''
+  for a_line in fileinput.input():
+    jsonStr = jsonStr + ' ' + a_line.strip()    
+  jsonObj = json.loads(jsonStr)  
+  print json.dumps(jsonObj, sort_keys=True, indent=2) 
